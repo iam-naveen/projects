e10be772a0273988c34f659366cbd91a0d5e46bc
Naveen <imnaveenbharath@gmail.com>
2022-03-29 20:39:12 +0530

    Redesign Schedule

---
+++ Online Class Schedule/Schedule.js
@@ -0,0 +1,21 @@
+        function startTime() {
+        //   const today= new Date();
+        //   let h = today.getHours();
+        //   let m = today.getMinutes();
+        //   let s = today.getSeconds();
+        //   m = checkTime(m);
+        //   s = checkTime(s);
+        //   h = checkTime(h);
+        //   document.getElementById('clock').innerHTML =  h + " : " + m + " : " + s;
+        //   setTimeout(startTime, 1000);
+        // }
+        // function checkTime(i) {
+        //   i < 10 ? i="0"+i: i=0+i;
+        //   return i;
+        // }
+
+        var today= new Date();
+        let d = today.toLocaleTimeString(['en-in'], hour12=true);
+        document.getElementById("clock").innerHTML = d;
+        setTimeout(startTime, 100);
+        }
