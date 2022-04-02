45f6fbf9e2835fce981025bc3434c19e8267ad63
Naveen <imnaveenbharath@gmail.com>
2022-04-02 17:25:49 +0530

    Added a Random Number Generator Project

---
+++ RandomGenerator/main.js
@@ -0,0 +1,20 @@
+var randomNum;
+
+function randomNumber(){
+    
+    showcopy();
+    var power = document.querySelector('#input').value;
+    randomNum = Math.floor(Math.random()*(10**power));
+    document.querySelector('#num').textContent = randomNum;
+}
+
+function showcopy(){
+    document.getElementById('copyButton').style.visibility = 'visible';
+    console.log("launched");
+}
+
+function copy(){
+    navigator.clipboard.writeText(randomNum);
+    alert("Number Copied");
+    console.log("COPIED");
+}
